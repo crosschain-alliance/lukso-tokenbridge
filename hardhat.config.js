6d9a06781332b61a0375bf203d17838cea025807
zengzengzenghuy <tayzh2799@gmail.com>
2024-07-09 00:35:51 +0200

    chore: config and patch

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,29 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-tracer");
+require("./task/e2e");
+require("hardhat-change-network");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    // hardhat: {
+    //   forking: {
+    //     url: "http://127.0.0.1:8545", //process.env.GNOSIS_JSON_RPC_URL,
+    //   },
+    //   chainId: 1,
+    // },
+
+    fmainnet: {
+      url: "http://127.0.0.1:8545",
+    },
+    flukso: {
+      url: "http://127.0.0.1:8544",
+    },
+  },
   solidity: {
     version: "0.8.24",
     settings: {
       viaIR: true,
     },
-  }
-
+  },
 };
